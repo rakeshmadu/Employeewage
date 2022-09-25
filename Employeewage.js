7aa099e6000bd6df8ad26593f1e48b0ccfd4b7da
rakeshmadu <rakeshmadu24.com>
2022-09-25 15:23:33 +0530

    check whether employee present or not

---
+++ Employeewage.js
@@ -0,0 +1,8 @@
+const IS_ABSENT = 0
+let employeeCheck = Math.floor(Math.random() * 10) % 2;
+
+if (employeeCheck == IS_ABSENT) {
+    console.log("Employee is ABSENT");
+} else {
+    console.log("Employee is PRESENT");
+}
